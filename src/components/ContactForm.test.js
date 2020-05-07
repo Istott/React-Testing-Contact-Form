a43a8caaf38e6f5a61dfbbbc0412e306ccb623f5
isaac stott <isaac.cloyd@gmail.com>
2020-05-07 16:05:37 -0600

    met mvp, trying to get stretch

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import { act } from 'react-dom/test-utils';
+import ContactForm from "./ContactForm";
+
+test("renders App without crashing", () => {
+  render(<ContactForm />);
+});
+
+test('contact form adds new users to user list', () => {
+    const { getByLabelText, getByTestId } = render(<ContactForm />)
+
+    const firstNameInput = getByLabelText(/First Name*/i);
+    const lastNameInput = getByLabelText(/Last Name*/i);
+    const emailInput = getByLabelText(/Email*/i);
+    const messageInput = getByLabelText(/Message/i);
+
+    fireEvent.change(firstNameInput, {target: {value: 'Edd'}});
+    fireEvent.change(lastNameInput, {target: {value: 'burke'}});
+    fireEvent.change(emailInput, {target: {value: 'bluebill1049@hotmail.com'}});
+    fireEvent.change(messageInput, {target: {value: 'Edd is a beastly individual'}});
+
+
+    const termsInput = getbyLabelText(/terms/i)
+    
+    fireEvent.click(termsInput);
+
+    const submitButton = getByTestId(/submit-button/i);
+
+    act(() => {
+        fireEvent.click(submitButton);
+
+    })
+
+
+})
